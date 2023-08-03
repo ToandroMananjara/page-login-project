1f11e2c00f5343bf7265e5f21e7eaf3ee895c20f
ToandroMananjara <mahasalotra@gmail.com>
2023-08-03 11:13:05 +0000

    message affiché dans le contenu html

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,30 +22,44 @@ const customer = [
         mdp:"rakoto1234"
     }
 ];
+let message
 
-function afficheMessage(container){
-    let message = document.createElement('div')
+let container1 = document.querySelector('.container-item1')
+function createMessage(container){
+
+    message = document.createElement('div')
     message.classList.add('message')
     container.appendChild(message)
 }
+function afficheMessage(text){
+     message.innerHTML=text
+}
+let success = "bienvenue"
+let  isAdminFound = false
 function test (email,mdp,list){
-    let i = 0
-    isAdminFound=false
-    while(i < list.length){
-        if(email.value==list[i].email){
-           if (mdp.value==list[i].mdp){
-            alert('Bievenue'+' '+email.value) 
+    
+    let mesTrue
+    let mesFalse
+    // isAdminFound=false    
+    list.forEach(element =>{
+        if(email.value==element.email){
+           if (mdp.value==element.mdp){
+                mesTrue = `Bienvenue ${email.value}`
+               afficheMessage(mesTrue)
+
             isAdminFound = true
-            break
         }
             
      }
-     i++
+        
+    });
+    if(!isAdminFound){
+        // alert('Email ou mot de passe incorrect')
+        mesFalse = "Email ou mot de passe incorrect"
+        afficheMessage(mesFalse)
     }
- if(!isAdminFound){
-                   alert('Email ou mot de passe incorrect')
+    
     }
-}
 function inputVide(input){
     if(input.value===""){
         input.style.border="solid 1px red"
@@ -58,23 +72,27 @@ function inputVide(input){
 }
 let inputMailAdmin = document.getElementById('input-mail')
 let inputMdpAdmin = document.getElementById('input-mdp')
-
 let btnAdmin = document.getElementById('btn-admin')
 btnAdmin.addEventListener('click',function(){
+    createMessage(container1)
     inputVide(inputMailAdmin)
     inputVide(inputMdpAdmin)
     setTimeout(function(){
         test(inputMailAdmin,inputMdpAdmin,admin)
-    },500)
+
+    },50)
 })
 let inputMailCustomer = document.getElementById('input-mail-customer')
 let inputMdpCustomer = document.getElementById('input-mdp-customer')
+let container2 = document.querySelector('.container-item2')
 let btnCustomer=document.getElementById('btn-customer')
 btnCustomer.addEventListener('click',function(){
-
+    createMessage(container2)
+    message.style.color="var(--bleu)"
+    // message.style.boxShadow =" 5px 5px 5px grey";
     inputVide(inputMailCustomer)
     inputVide(inputMdpCustomer)
     setTimeout(function(){
         test(inputMailCustomer,inputMdpCustomer,customer)
-                },500)
+                },50)
             })
